bf3a3486d85b8f2e471f0699a1b51f78187ac6c7
INS140 <IsaacNSaettel@gmail.com>
2023-03-17 01:30:33 -0700

    suspense

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
-import AboutUs from './components/AboutUs'
-import Packages from './components/Packages'
+import { Bars } from 'react-loader-spinner'
+const AboutUs = React.lazy(() => import('./components/AboutUs'))
+const Packages = React.lazy(() => import('./components/Packages'))
 
 function App() {
   return (
@@ -9,8 +10,15 @@ function App() {
       <h1>Lonzo's Travel Agency</h1>
       <h2>Make your travel dreams come true</h2>
       <div className="contents">
+        <Suspense fallback={<Bars
+          color='#FF00FF'
+          height={200}
+          width={200}
+          style={{margin: '0 auto'}}
+        />}>
           <AboutUs />
           <Packages />
+        </Suspense>
       </div>
     </div>
   );
